cfc2b21cb11d8f194745252ed84281753c52e6ed
Игорь Жванко < >
2016-09-10 00:36:26 +0300

    insert data added

---
+++ insert_manufacturer.sql
@@ -0,0 +1,11 @@
+Insert Into Manufacturer Values
+('Quimica Montpellier', 	'Argentina', 	'Телефон: 8(032)71-24-66', 	'test@gmail.com'),
+('Bayer AG', 	'USA', 	'Телефон: 8(032)22-18-95', 	'test@gmail.com'),
+('World of Health Biotech Co.Ltd', 	'USA', 	'Телефон: 8(032)97-38-59', 	'test@gmail.com'),
+('GlaxoSmithKline C.A.', 	'Venezuela', 	'Телефон: 8(032)63-33-00', 	'test@gmail.com'),
+('Канонфарма продакшн', 	'Russia', 	'Телефон: 8(032)62-18-69', 	'test@gmail.com'),
+('WIRUD GmbH', 	'Germany', 	'Телефон: 8(032)33-23-35', 	'test@gmail.com'),
+('Laboratorios Elmor', 	'France', 	'Телефон: 8(032)63-33-00', 	'test@gmail.com'),
+('Quimica Suiza', 	'Spain', 	'Телефон: 8(032)67-24-67', 	'test@gmail.com'),
+('Arco Pharmaceuticals Inc.', 	'USA', 	'Телефон: 8(032)91-15-81', 	'test@gmail.com'),
+('Вилар', 	'Russia', 	'Телефон: 8(032)35-15-91', 	'test@gmail.com')
